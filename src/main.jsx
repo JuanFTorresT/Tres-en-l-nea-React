2f298a1fb46911fe81dc176ab220de09b40a9dca
Felipe Torres <juanftorrest@gmail.com>
2024-08-24 07:27:48 -0500

    se creó el tablero y se le agregó estado al boton X

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//crea la raiz del dom donde se van a renderizar los componentes de react y luego los renderiz con .render
+createRoot(document.getElementById('root')).render(
+  //stricmode renderiz tus componentes una vez más con el objetivo de encontrar errores
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
